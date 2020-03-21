f7c709596d375f44b505d021e7096ef08cfc7bd1
maimaiti aziguli <arzuyum507@gmail.com>
2020-03-20 22:10:04 -0700

    first commit

---
+++ src/test/java/com/GitPush/Java.java
@@ -0,0 +1,12 @@
+package com.GitPush;
+
+public class Java {
+    public static void main(String[] args) {
+        String str="selenium";
+        String reverse="";
+        for (int i=str.length()-1; i>=0; i--){
+            reverse+=str.substring(i,i+1);
+        }
+        System.out.println("reverse = " + reverse);
+    }
+}
